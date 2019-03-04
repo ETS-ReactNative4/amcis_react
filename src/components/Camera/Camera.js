e1503f5516d407c8775ffa091a7e9a740d1ded86
SiLenT_Tz <shadow-jan@hotmail.de>
2019-03-04 14:45:17 +0100

    reverted

--- src/components/Camera/Camera.js
+++ src/components/Camera/Camera.js
@@ -1,23 +1,20 @@
-import React, { Component } from "react";
-import Camera from "react-html5-camera-photo";
-import "react-html5-camera-photo/build/css/index.css";
-import ReactWebComponent from "react-web-component";
+import React, { Component } from 'react';
+import Camera from 'react-html5-camera-photo';
+import 'react-html5-camera-photo/build/css/index.css';
+import ReactWebComponent from 'react-web-component';
 
 class CameraComponent extends HTMLElement {
-  onTakePhoto(dataUri) {
-    console.log("takePhoto");
-  }
+    onTakePhoto(dataUri){
+        console.log('takePhoto');
+    }
 
-  connectedCallback() {
-    ReactDOM.render(
-      <Camera
-        onTakePhoto={dataUri => {
-          this.onTakePhoto(dataUri);
-        }}
-      />
-    );
-  }
-}
-customElements.define("camera-component-react",CameraComponent);
-export default CameraComponent;
-ReactWebComponent.create(<CameraComponent />, "camera-component-react");
+    render(){
+        return(
+            <Camera onTakePhoto = {(dataUri) => {this.onTakePhoto(dataUri);}}
+       />
+       );
+        
+    }
+} 
+export default CameraComponent
+ReactWebComponent.create(<CameraComponent />, 'camera-component-react');
